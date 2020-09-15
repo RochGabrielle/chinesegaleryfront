d6fd00982779de72f7aba1a9047461c5c7ac2aa8
rochGabrielle <roch.gab@gmail.com>
2020-09-15 17:12:16 +0200

    add filter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -54,6 +54,43 @@ imgSrc = GlobalConstants.imgURL;
   											}]);
   }
 
+filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  console.log(x);
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    this.w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) this.w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+ w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
   ngOnInit(): void {
 
     this.slideService.slideshow('fr_fr').subscribe(
@@ -72,11 +109,7 @@ imgSrc = GlobalConstants.imgURL;
   console.log(Object.values(response));
   console.log(this.imgSrc);
   this.gallery = Object.values(response);
-
         }
-      );
-
-  	
+      );  	
   }
-
 }
